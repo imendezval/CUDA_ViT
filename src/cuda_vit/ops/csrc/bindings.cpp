c5419475b08194dcf9793a0c968fd1755d3a6173
imendezval <imendezval@stud.hs-heilbronn.de>
2026-07-07 14:06:26 +0200

    removes old leftover bindings file

--- src/cuda_vit/ops/csrc/bindings.cpp
+++
@@ -1,7 +0,0 @@
-#include <torch/extension.h>
-
-torch::Tensor vector_add_cuda(torch::Tensor a, torch::Tensor b);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("vector_add", &vector_add_cuda, "Vector addition (CUDA)");
-}
